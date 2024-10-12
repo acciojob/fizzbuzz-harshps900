63bbaea551a1f0c8f740aff048e9c6f12e19e3d6
harshps900 <151460810+harshps900@users.noreply.github.com>
2024-10-12 21:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+for (let i = 0; i < 20; i++){
+	if(i%3==0 && i%5==0){
+		console.log(i,"FizzBuzz");
+	}
+	else if(i%3==0){
+		console.log(i,"Fizz");
+	}
+	else if( i%5==0){
+		console.log(i,"Buzz");
+	}
+}
